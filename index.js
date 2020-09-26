a431c839a78f667a3b6b36012a620bd552666127
AAGaming <aa@bigdumb.gq>
2020-09-25 23:53:41 -0400

    working yes

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const { getModule } = require('powercord/webpack');
-const { PopoutWindow } = require('powercord/components');
 const { inject, uninject } = require('powercord/injector');
 const { Plugin } = require('powercord/entities');
-const { React, ReactDOM } = require('powercord/webpack');
 const path = require('path');
-const { remote: { BrowserWindow } } = require('electron');
-const Notif = require('./components/Notification');
+const { remote: { BrowserWindow, getCurrentWindow }, ipcRenderer } = require('electron');
+const { getUser } = getModule([ 'getCurrentUser' ], false);
+const { getChannel } = getModule([ 'getChannel' ], false);
+const { getGuild } = getModule([ 'getGuild' ], false);
+const { markdownToHtml } = getModule([ 'markdownToHtml' ], false);
+const transition = getModule([ 'transitionTo' ], false);
 module.exports = class BetternNotifs extends Plugin {
   startPlugin () {
     const showNotifm = getModule([ 'makeTextChatNotification' ], false);
@@ -13,26 +15,48 @@ module.exports = class BetternNotifs extends Plugin {
 
     inject('betterNotifs', showNotifm, 'makeTextChatNotification', (args) => {
       console.log(args);
+
+      const parsedArgs = JSON.parse(JSON.stringify(args));
+      parsedArgs[1].content = markdownToHtml(parsedArgs[1].content
+        .replace(/<@!?(\d+)>/g, (_, p) => `@${getUser(p).username}`)
+        .replace(/<#(\d+)>/g, (_, p) => `#${getChannel(p).name}`)
+        .replace(/<@&(\d+)>/g, (_, p) => `@${getGuild(args[0].guild_id).roles[p].name}`)
+        .replace(/\|\|.+\|\|/g, (_, p) => '<spoiler>')); // Thanks Ben!
       setTimeout(() => {
-        const win = new BrowserWindow({ width: 800,
-          height: 600,
+        const win = new BrowserWindow({
+          height: 120,
+          width: 400,
+          x: -900000,
+          y: -900000,
+          alwaysOnTop: true,
+          skipTaskbar: true,
+          frame: false,
           webPreferences: {
             nodeIntegration: true,
-            nodeIntegrationInWorker: true
-          } });
-        const discordNotif = {
-          React,
-          ReactDOM
-        };
-        const discordJson = JSON.stringify(discordNotif, (key, value) => {
-          if (typeof value === 'function') {
-            return value.toString();
+            nodeIntegrationInWorker: true,
+            enableRemoteModule: true
           }
-          return value;
         });
-        win.openDevTools();
-        win.loadFile(path.join(__dirname, 'notifWindow', 'index.html'), { query: { modules: discordJson } });
-      }, 1000);
+        console.log(parsedArgs[1].content);
+        win.setResizable(false);
+        win.webContents.on('did-finish-load', () => {
+          ipcRenderer.on(
+            'better-notifs-jump',
+            (event, message) => {
+              if (message) {
+                const guild = getGuild(message[0].guild_id);
+                transition.transitionTo(`/channels/${guild ? guild.id : '@me'}/${message[0].id}/${message[1].id}`); // Again, thanks Ben!
+                resolve(message);
+              }
+              reject();
+            }
+          );
+          win.webContents.executeJavaScript(`
+            window.windowId = ${getCurrentWindow().webContents.id};
+          `);
+        });
+        win.loadFile(path.join(__dirname, 'notifWindow', 'index.html'), { query: { message: JSON.stringify(parsedArgs) } });
+      }, 0);
       return args;
     }, true);
   }
