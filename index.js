39dc3be9e38857d33561038f0b68e87ef339ec78
AAGaming <aa@bigdumb.gq>
2020-09-26 20:17:19 -0400

    e

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ module.exports = class BetterNotifs extends Plugin {
         });
         // show window without setting focus
         win.showInactive();
-        getCurrentWindow.focus()
+        getCurrentWindow.focus();
         win.loadFile(path.join(__dirname, 'notifWindow', 'index.html'), { query: { message: JSON.stringify(parsedArgs) } });
       }, 0);
       inject('betterNotifs-blocker', shouldDisplayNotifications, 'shouldDisplayNotifications', (args) => false, true);
