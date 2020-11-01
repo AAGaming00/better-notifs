66036114b4a12b232a1cb35e891c742582aecaad
AAGaming <aa@bigdumb.gq>
2020-10-31 22:24:52 -0400

    console spam bad :angery:

--- wrapper.js
+++ wrapper.js
@@ -1,5 +1,4 @@
 module.exports = `console.log('loading BN')
-console.log(process.argv);
 if (process.argv[1] === 'bn-show') {
     process.on('message', (data) => {
     require(process.argv[2])(data);
